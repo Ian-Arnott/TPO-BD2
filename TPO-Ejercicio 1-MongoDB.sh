2bd6fdde9e35cbcb69bccb3d3b580f52a8e4fdd3
David Itcovici <daviditco4@gmail.com>
2024-06-11 19:17:45 -0300

    Create TPO-Ejercicio 1-MongoDB.sh

---
+++ TPO-Ejercicio 1-MongoDB.sh
@@ -0,0 +1,4 @@
+echo 'db.albums.aggregate([{$group: {_id: "$Year", count: {$sum: 1}}}, {$sort: {count: -1}}]);
+db.albums.updateMany({}, [{$set: {score: {$subtract: [501, "$Number"]}}}]);
+db.albums.aggregate([{$group: {_id: "$Artist", score: {$sum: "$score"}}}, {$sort: {score: -1}}]);' |
+mongosh "mongodb://localhost:27017"
